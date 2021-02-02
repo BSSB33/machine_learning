f1fba8c0d1f9d41665db73ed0f7defa83b153fa5
Vitrai Gábor <vitraig@gmail.com>
2021-02-02 19:59:48 +0100

    Missing Images and Own code added

--- Machine Learning A-Z Hands-On Python/Part 2 Regression/Section 2 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z Hands-On Python/Part 2 Regression/Section 2 - Multiple Linear Regression/multiple_linear_regression.py
@@ -8,15 +8,17 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-print(X)
+y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-print(X)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+LabelEncoder_X = LabelEncoder()
+X[:, 3] = LabelEncoder_X.fit_transform(X[:, 3])
+oneHotEncoder = OneHotEncoder()
+X = oneHotEncoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -29,5 +31,22 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Building the optimal model using Backward Elimination
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
