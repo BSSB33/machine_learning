ab4f3d8859a9d4edc94981c284c3274187600f9a
Gábor Vitrai <vitraig@gmail.com>
2021-12-27 17:46:33 +0100

    Exportable Conditions & Therapies

--- Data Mining/condition.py
+++ Data Mining/condition.py
@@ -8,11 +8,11 @@ class Condition:
 
 # New class to support the extra attributes between class "Condition" and the asked attributes in patent.conditions
 class Patent_condition: 
-    def __init__(self, id, diganosed, cured, id_of_condition):
+    def __init__(self, id, diagnosed, cured, kind):
         self.id = id
-        self.diagnosed = diganosed
+        self.diagnosed = diagnosed 
         self.cured = cured
-        self.kind = id_of_condition
+        self.kind = kind
 
 class Therapy:
     def __init__(self, id, name, type):
@@ -22,20 +22,20 @@ class Therapy:
 
 # New class to support the extra attributes between class "Terapy" and the asked attributes in "trials"
 class Patient_therapy:
-    def __init__(self, id, start, end, id_of_condition, id_of_therapy, successful):
+    def __init__(self, id, start, end, condition, therapy, successful):
         self.id = id
         self.start = start
         self.end = end
-        self.condition = id_of_condition
-        self.therapy = id_of_therapy
+        self.condition = condition
+        self.therapy = therapy
         self.successful = successful
   
 class Patient:
-    def __init__(self, id, name, patient_conditions, patient_therapies):
+    def __init__(self, id, name, conditions, trials):
         self.id = id
         self.name = name
-        self.conditions = patient_conditions
-        self.trials = patient_therapies
+        self.conditions = conditions
+        self.trials = trials
 
 
 def load_demo_patients():
@@ -45,34 +45,68 @@ def load_demo_patients():
     t2 = Therapy("Th2", "Cough Syrup Quibron", "Sugar")
     p_c1 = Patent_condition("pc1", 20210915, 20210915, "Cond1")
     p_c2 = Patent_condition("pc2", 20210602, 20210930, "Cond2")
+    p_c3 = Patent_condition("pc3", 20210603, 20210905, "Cond2")
     p_t1 = Patient_therapy("tr1", 20210915, 20211215, "pc1", "Th1", 10)
     p_t2 = Patient_therapy("tr2", 20210102, 20210130, "pc2", "Th2", 100)
+    p_t3 = Patient_therapy("tr3", 20210102, 20210130, "pc2", "Th3", 90)
     patient1 = Patient(1, "John", [p_c1.__dict__, p_c2.__dict__], [p_t1.__dict__, p_t2.__dict__])
-    patient2 = Patient(1, "Peter", [p_c1.__dict__], [p_t1.__dict__])
+    patient2 = Patient(1, "Peter", [p_c3.__dict__], [p_t3.__dict__])
 
     conditions = [condition.__dict__ for condition in [c1, c2]]
     therapies = [therapy.__dict__ for therapy in [t1, t2]]
     patients = [patient.__dict__ for patient in [patient1, patient2]]
     return conditions, therapies, patients
 
-def export_dataset(conditions, therapies, patients):
-    with open('dataset.json', 'w') as outfile:
+def export_demo_dataset(file, conditions, therapies, patients):
+    with open(file, 'w') as outfile:
         json.dump({"Conditions": conditions, "Therapies": therapies, "Patients": patients}, outfile)
+    outfile.close()
+
+def export_dataset(file, conditions, therapies, patients):
+    printable_patients = []
+    for patient in patients:
+        printable_patients.append(Patient(patient.id, patient.name, [condition.__dict__ for condition in patient.conditions], [trial.__dict__ for trial in patient.trials]))
+
+    #TODO Patients is not serializable, rewrite printable_patients below if possible
+    with open(file, 'w') as outfile:
+        json.dump({"Conditions": [condition.__dict__ for condition in conditions], "Therapies": [therapy.__dict__ for therapy in therapies], "Patients": printable_patients}, outfile)
     outfile.close()
 
 def import_dataset():
     conditions = []
+    therapies = []
+    patients = []
+    patient_conditions = []
+    patient_therapies = []
     with open('dataset.json', 'r') as json_file:
         data = json.load(json_file)
         for condition in data["Conditions"]:
             obj = json.loads(json.dumps(condition), object_hook=lambda d: Condition(**d))
             conditions.append(obj)
-            print(obj.__dict__)
         for therapy in data["Therapies"]:
             obj = json.loads(json.dumps(therapy), object_hook=lambda d: Therapy(**d))
-            conditions.append(obj)
-            print(obj.__dict__)
+            therapies.append(obj)
+        for patient in data["Patients"]:
+            current_patient_conditions = []
+            current_patient_therapies = []
+            for patient_condition in patient["conditions"]:
+                obj = json.loads(json.dumps(patient_condition), object_hook=lambda d: Patent_condition(**d))
+                patient_conditions.append(obj)
+                current_patient_conditions.append(obj)
+            for patient_therapy in patient["trials"]:
+                obj = json.loads(json.dumps(patient_therapy), object_hook=lambda d: Patient_therapy(**d))
+                patient_therapies.append(obj)
+                current_patient_therapies.append(obj)
+            p = Patient(patient["id"], patient["name"], current_patient_conditions, current_patient_therapies)
+            patients.append(p)
     json_file.close()
+    return conditions, therapies, patients
+
+""" def generate_printable_patients(patients):
+    printable_patients = []
+    for patient in patients:
+        printable_patients.append(Patient(patient.id, patient.name, [condition.__dict__ for condition in patient.conditions], [trial.__dict__ for trial in patient.trials]))
+    return printable_patients """
     
 if __name__ == "__main__":
     """ 
@@ -81,9 +115,12 @@ if __name__ == "__main__":
     Input 3: A condition c[q]
     Output: A therapy th[ans]
     """
-    #conditions, therapies, patients = load_demo_patients()
-    #export_dataset(conditions, therapies, patients)
-    import_dataset()
+    conditions, therapies, patients = load_demo_patients()
+    export_demo_dataset("dataset.json", conditions, therapies, patients)
+    conditions, therapies, patients = import_dataset()
+    #patients = generate_printable_patients(patients)
+    #print([condition.__dict__ for condition in conditions])
+    export_dataset("dataset_new.json", conditions, therapies, patients)
 
 
  
